14e69486185408b76dcc38c7f3d1d7307577a1ae
Aadya-1188 <aadyapathak2509@gmail.com>
2025-07-26 12:19:28 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,9 @@
+a = 12
+b = 45
+c = 5
+d = 100
+e = 76
+sum = a + b + c + d + e
+print("The Sum Of The Five Numbers Are" , sum)
+average = sum/5
+print("The Average Of The Five Numbers Are" ,average)
