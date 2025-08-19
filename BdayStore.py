ccff4c739fb8ccf99d93b12ffeb967c06fafc106
Aadya-1188 <aadyapathak2509@gmail.com>
2025-08-19 20:19:08 +0530

    Add files via upload

---
+++ BdayStore.py
@@ -0,0 +1,13 @@
+# storing birthdays in variables
+aadya = "10-01-2010"
+adwika = "15-03-2011"
+vaidya = "20-05-2012"
+advit = "25-07-2013"
+ahshaj = "30-09-2014"
+
+# printing them
+print("Aadya's Birthday:", aadya)
+print("Adwika's Birthday:", adwika)
+print("Vaidya's Birthday:", vaidya)
+print("Advit's Birthday:", advit)
+print("Ahshaj's Birthday:", ahshaj)
