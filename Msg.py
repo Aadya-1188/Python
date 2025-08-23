3f790db5199c0b551e88c0e10af8dc289356b60d
Aadya-1188 <aadyapathak2509@gmail.com>
2025-08-23 11:07:27 +0530

    Add files via upload

---
+++ Msg.py
@@ -0,0 +1,10 @@
+# Program to convert a word into uppercase
+
+# Taking input in lowercase
+word = input("Enter the word: ")
+
+# Converting to uppercase
+uppercase_word = word.upper()
+
+# Displaying result
+print("Word in uppercase:", uppercase_word)
